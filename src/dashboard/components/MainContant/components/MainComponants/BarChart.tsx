7446abee855c968e04cd899d7ba053a96a059de8
Manar Jber <mohamadmanar960@gmail.com>
2024-05-06 18:04:21 +0300

    charts

--- src/dashboard/components/MainContant/components/MainComponants/BarChart.tsx
+++ src/dashboard/components/MainContant/components/MainComponants/BarChart.tsx
@@ -1,20 +1,70 @@
-import dayjs from 'dayjs';
-import img from '../../../../../assets/images/bar.png';
-import dashboardStore from '../../../../../context/dashboardStore';
+import Chart from 'react-apexcharts';
+
+import TransactionService, { Transaction } from '../../../../transactions-api';
 
 const BarChart = () => {
-  const selectedDate = dashboardStore((state) => state.selectedDate);
-  const formattedDate = selectedDate
-    ? dayjs(selectedDate).format('MM/DD/YYYY')
-    : dayjs().format('MM/DD/YYYY')
+  const getMonthlyAmounts = (): number[] => {
+    const transactions = TransactionService.getAllTransactions();
+
+    const monthlyAmounts: number[] = Array.from({ length: 12 }).fill(0) as number[];
+
+    transactions.forEach((transaction: Transaction) => {
+      const transactionDate = new Date(transaction.date);
+      const month = transactionDate.getMonth();
+      const amount = transaction.amount;
+      monthlyAmounts[month] += amount;
+    });
+
+    return monthlyAmounts;
+  };
+
+  const monthlyAmounts: number[] = getMonthlyAmounts();
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  const colors = ['#ff8bb9'];
 
   return (
-    <div className="container-fluid">
+    <div className="container-fluid mb-5">
       <div className="row">
         <div className="col-lg-12">
-          <section className="column-container flex-column bg-white rounded-4 d-flex align-items-center justify-content-center overflow-hidden shadow-sm">
-            <h5 className="pt-5">Date is: {formattedDate}</h5>
-            <img src={img} alt="bar chart" className="pb-5" />
+          <section className="column-container bg-white rounded-4 d-flex flex-column align-items-center justify-content-center shadow-sm">
+            <h5 className="mt-3">Expenses Each Month</h5>
+            <div className="barChart">
+              <Chart
+                options={{
+                  xaxis: {
+                    categories: months
+                  },
+                  plotOptions: {
+                    bar: {
+                      borderRadius: 8
+                    }
+                  },
+                  colors: colors
+                }}
+                series={[
+                  {
+                    name: 'Amount',
+                    data: monthlyAmounts
+                  }
+                ]}
+                type="bar"
+                width={740}
+              />
+            </div>
           </section>
         </div>
       </div>
